d7d18a518cf876957e266db3b485f2d288ec34df
Fredrik Meyer <hrmeyer@gmail.com>
2017-10-16 22:22:09 +0200

    Refactor to the worse

---
+++ src/main/java/com/fredrikmeyer/App.java
@@ -0,0 +1,93 @@
+package com.fredrikmeyer;
+
+import javax.imageio.ImageIO;
+import javax.imageio.plugins.jpeg.JPEGHuffmanTable;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.fredrikmeyer.picturelanguage.*;
+import com.fredrikmeyer.picturelanguage.Frame;
+import com.fredrikmeyer.picturelanguage.Painter;
+import oracle.jvm.hotspot.jfr.JFR;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        File imageFile = new File("./src/resources/moss.png");
+        ArrayList<Segment> segments = new ArrayList<Segment>();
+        //segments = segmentListFromFile(new File("./src/resources/smiley.txt"));
+        //Painter p = new Painter(Frame.IDENTITY,  segments);
+        //Painter p = new Painter(new Frame(Vector.ORIGIN, new Vector(0.5, 0), new Vector(0.5,1)), imageFile);
+        //Painter p = new Painter(Frame.IDENTITY, imageFile);
+        //BufferedImage image = p.getImage();
+
+        App gui = new App();
+        System.out.println( System.getProperty("user.dir"));
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        DrawPanel drawPanel = new DrawPanel("./src/main/java/resources/moss.png");
+
+        frame.getContentPane().add(drawPanel);
+        frame.setSize(drawPanel.getWidth(), drawPanel.getHeight());
+        frame.setVisible(true);
+    }
+
+    public static ArrayList<Segment> segmentListFromFile(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        ArrayList<Segment> segmentList = new ArrayList<>();
+        while (scanner.hasNext()) {
+            double x1 = Double.valueOf(scanner.next());
+            double y1 = Double.valueOf(scanner.next());
+            double x2 = Double.valueOf(scanner.next());
+            double y2 = Double.valueOf(scanner.next());
+            Vector start = new Vector(x1, y1);
+            Vector end = new Vector(x2, y2);
+            segmentList.add(new Segment(start, end));
+        }
+        return segmentList;
+    }
+
+
+    // TODO la painter-klassen returnere et JPanel, som tegnes i hovedklassen
+    // også: lær maven, for nå skjønner jeg ikke stort...
+    class DrawPanel extends JPanel {
+        private BufferedImage bufferedImage;
+
+        public DrawPanel(String filename) {
+            loadImageFile(filename);
+        }
+
+        private void loadImageFile(String filename) {
+            try {
+                File imagefile = new File(filename);
+                bufferedImage = ImageIO.read(imagefile);
+            } catch (IOException e) {
+                System.out.println("Could not find file.");
+            }
+        }
+
+        public void paintComponent(Graphics g) {
+            g.drawImage(bufferedImage, 0, 0, this);
+        }
+
+        public int getWidth() {
+            return bufferedImage.getWidth();
+        }
+
+        public int getHeight() {
+            return bufferedImage.getHeight();
+        }
+    }
+}
