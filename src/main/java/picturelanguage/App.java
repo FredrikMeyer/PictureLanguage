8dd581014e48962eddbc9daba60cd443734802fe
Fredrik Meyer <hrmeyer@gmail.com>
2018-03-27 23:45:03 +0200

    Replace ImageJ by BufferedImage

--- src/main/java/picturelanguage/App.java
+++ src/main/java/picturelanguage/App.java
@@ -1,34 +1,28 @@
 package picturelanguage;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import javax.swing.*;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        //ip.setColor(Color.BLUE);
-        //ip.setLineWidth(4);
-        //ip.drawRect(10, 10, imp.getWidth() - 20, imp.getHeight() - 20);
-        //imp.show();
-        //System.out.println(imp.getBitDepth());
-        //ImagePlus imp2 = IJ.createImage("Tittel", 400, 400, 1, 24);
-        //imp2.show();
-
-        File file = fromResourceFolder("smiley.txt");
+        File file = fileFromResourceFolder("picturelanguage/smiley.txt");
         List<Segment> segments = Segment.segmentsFromFile(file);
-        Painter p =  Painter.fromSegments(segments); //fromFile("./src/main/java/resources/moss.png");
-        Painter p2 = Painter.fromFile(fromResourceFolder("picturelanguage/moss.png"));
-        //p2.show();
+        //Painter p =  Painter.fromSegments(segments); //fromFile("./src/main/java/resources/moss.png");
+        Painter p2 = Painter.fromFile(fileFromResourceFolder("picturelanguage/moss.png"));
+        Painter hFlipped = p2.horizontalFlip();
+        Painter vFlipped = p2.verticalFlip();
 
-        p.beside(p).show();
-
+        Painter besides = p2.beside(hFlipped);
+        besides.verticalFlip().above(besides).displayImage(":)");
     }
 
-    private static File fromResourceFolder(String filename) {
-        System.out.println(System.getProperty("user.dir"));
+    private static File fileFromResourceFolder(String filename) {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         return new File(Objects.requireNonNull(classLoader.getResource(filename)).getFile());
     }
--- src/main/java/picturelanguage/Painter.java
+++ src/main/java/picturelanguage/Painter.java
@@ -6,9 +6,12 @@ import ij.plugin.Concatenator;
 import ij.process.ImageProcessor;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -17,12 +20,7 @@ import java.util.List;
 public class Painter {
     private static final int SIZE = 400;
 
-    private ImagePlus image;
     private BufferedImage bufferedImage;
-
-    public Painter(ImagePlus image) {
-        this.image = image;
-    }
 
     public Painter(BufferedImage bufferedImage) {
         this.bufferedImage = bufferedImage;
@@ -34,13 +32,13 @@ public class Painter {
     }
 
     public static Painter fromNumber(int n) {
+        BufferedImage img = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_RGB);
+        img.createGraphics();
+        Graphics2D g = (Graphics2D) img.getGraphics();
+        g.setColor(new Color(n, n, n));
+        g.fillRect(0, 0, SIZE, SIZE);
 
-
-        ImagePlus image = IJ.createImage("Tittel", SIZE, SIZE, 1, 24);
-        ImageProcessor imageProcessor = image.getProcessor();
-        imageProcessor.setColor(new Color(n, n, n));
-        imageProcessor.fill();
-        return new Painter(image);
+        return new Painter(img);
     }
 
     public static Painter fromSegments(List<Segment> segments) {
@@ -50,60 +48,80 @@ public class Painter {
         imageProcessor.fill();
         imageProcessor.setColor(Color.BLACK);
 
-        segments.forEach( seg -> {
+        segments.forEach(seg -> {
             int startX = resizeToFrameSize(seg.getStart().xCoordinate());
-            int startY = 400-resizeToFrameSize(seg.getStart().yCoordinate());
+            int startY = 400 - resizeToFrameSize(seg.getStart().yCoordinate());
             int endX = resizeToFrameSize(seg.getEnd().xCoordinate());
-            int endY = 400-resizeToFrameSize(seg.getEnd().yCoordinate());
+            int endY = 400 - resizeToFrameSize(seg.getEnd().yCoordinate());
             imageProcessor.drawLine(startX, startY, endX, endY);
         });
 
-        return new Painter(image);
+        return null;
     }
 
     public Painter horizontalFlip() {
-        ImagePlus tempImage = image.duplicate();
-        ImageProcessor imageProcessor = tempImage.getProcessor();
-        imageProcessor.flipHorizontal();
-        return new Painter(tempImage);
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+        BufferedImage newImg = new BufferedImage(width, height, bufferedImage.getType());
+        Graphics2D g = newImg.createGraphics();
+        g.drawImage(copyImage(bufferedImage), 0, 0, width, height, width, 0, 0, height, null);
+        return new Painter(newImg);
     }
 
     public Painter verticalFlip() {
-        ImagePlus tempImage = image.duplicate();
-        ImageProcessor imageProcessor = tempImage.getProcessor();
-        imageProcessor.flipVertical();
-        return new Painter(tempImage);
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+        BufferedImage newImg = new BufferedImage(width, height, bufferedImage.getType());
+        Graphics2D g = newImg.createGraphics();
+        g.drawImage(copyImage(bufferedImage), 0, 0, width, height, 0, height, width, 0, null);
+        return new Painter(newImg);
     }
 
     public Painter beside(Painter otherPainter) {
-        ImagePlus leftImage = image.duplicate();
-        ImageProcessor leftProcessor = image.getProcessor();
-        leftProcessor.scale(0.5,1);
-
-        ImagePlus rightImage = otherPainter.getImage().duplicate();
-        ImageProcessor rightProcessor = rightImage.getProcessor();
-        rightProcessor.scale(0.5,1);
-
-        Concatenator concatenator = new Concatenator();
-
-        ImagePlus res = concatenator.concatenate(leftImage, rightImage, true);
-        return new Painter(res);
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+        BufferedImage newImg = new BufferedImage(width, height, bufferedImage.getType());
+        Graphics2D g = newImg.createGraphics();
+        g.drawImage(copyImage(bufferedImage), 0, 0, width/2, height, null);
+        g.drawImage(copyImage(otherPainter.getImage()), width/2, 0, width/2, height, null);
+        return new Painter(newImg);
     }
 
-    public void show() {
-        image.show();
+    public Painter above(Painter otherPainter) {
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+        BufferedImage newImg = new BufferedImage(width, height, bufferedImage.getType());
+        Graphics2D g = newImg.createGraphics();
+        g.drawImage(copyImage(bufferedImage), 0, 0, width, height/2, null);
+        g.drawImage(copyImage(otherPainter.getImage()), 0, height/2, width, height/2, null);
+        return new Painter(newImg);
     }
 
-    public ImagePlus getImage() {
-        return image;
+    public BufferedImage getImage() {
+        return bufferedImage;
+    }
+
+    public void displayImage(String title) {
+        JFrame frame = new JFrame(title);
+        frame.setBounds(0, 0, bufferedImage.getWidth(), bufferedImage.getHeight());
+        frame.setVisible(true);
+        Graphics2D g = (Graphics2D) frame.getRootPane().getGraphics();
+        g.drawImage(bufferedImage, null, 0, 0);
     }
 
     private static int resizeToFrameSize(double number) {
         return (int) Math.floor(SIZE * number);
     }
 
-    // TODO: make a copy
     private BufferedImage applyAffineTransform(BufferedImage bImage, AffineTransformOp affineTransformOp) {
         return null;
     }
+
+    private static BufferedImage copyImage(BufferedImage source) {
+        ColorModel cm = source.getColorModel();
+        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = source.copyData(null);
+        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+    }
+
 }
